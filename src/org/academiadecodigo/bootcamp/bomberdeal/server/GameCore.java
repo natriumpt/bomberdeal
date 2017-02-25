10a3e24f2df19963d836f9b6e5815e2d41befe7a
Alexandre Ramos <natrium.pt@gmail.com>
2017-02-25 17:00:37 +0000

    Collision checker

--- src/org/academiadecodigo/bootcamp/bomberdeal/server/GameCore.java
+++ src/org/academiadecodigo/bootcamp/bomberdeal/server/GameCore.java
@@ -1,52 +1,25 @@
 package org.academiadecodigo.bootcamp.bomberdeal.server;
 
 import org.academiadecodigo.bootcamp.bomberdeal.server.gamefield.Field;
-import org.academiadecodigo.bootcamp.bomberdeal.server.gameobject.Fire;
-import org.academiadecodigo.bootcamp.bomberdeal.server.gameobject.interfaces.Collidable;
-import org.academiadecodigo.bootcamp.bomberdeal.server.gameobject.interfaces.Destroyable;
 import org.academiadecodigo.bootcamp.bomberdeal.server.gameobject.interfaces.Interactable;
+import org.academiadecodigo.bootcamp.bomberdeal.server.helper.CollisionChecker;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class GameCore implements Runnable {
 
-    private List<Interactable> interactables;
     private Field field;
+    private List<Interactable> interactables;
+    private CollisionChecker collisionChecker;
 
     public GameCore() {
         this.field = new Field();
+        this.interactables = Collections.synchronizedList(new ArrayList<>());
+        this.collisionChecker = new CollisionChecker();
     }
 
-    public boolean checkCollision(int x, int y) {
-
-        if (field.isWall(x, y)) {
-            return true;
-        }
-
-        for (Interactable collidable : interactables) {
-            if (collidable instanceof Collidable) {
-                if (collidable.getX() == x && collidable.getY() == y)
-                    return true;
-            }
-        }
-
-        return false;
-
-    }
-
-    public void processFire() {
-
-        for (Interactable fire : interactables) {
-            if (fire instanceof Fire) {
-                for (Interactable destroyable : interactables) {
-                    if (destroyable instanceof Destroyable) {
-                        destroyable.destroy();
-                    }
-                }
-            }
-        }
-
-    }
 
     @Override
     public void run() {
