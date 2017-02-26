f6c6e8d880652c43557449ee5f9f3b4358d05da8
Andre Leal <andre.mega7@gmail.com>
2017-02-26 11:22:03 +0000

    Server Game Loop start

---
+++ src/org/academiadecodigo/bootcamp/bomberdeal/server/Server.java
@@ -0,0 +1,37 @@
+package org.academiadecodigo.bootcamp.bomberdeal.server;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.SocketException;
+
+/**
+ * Created by andre on 2/26/2017.
+ */
+public class Server {
+
+    private ServerGame game;
+    private ServerSocket serverTPCSocket;
+    private DatagramSocket serverUDPSocket;
+
+    public Server(int portNumber) {
+
+        try {
+            serverTPCSocket = new ServerSocket(portNumber);
+            serverUDPSocket = new DatagramSocket(portNumber);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void startServer() {
+
+        game = new ServerGame();
+
+
+    }
+
+}
